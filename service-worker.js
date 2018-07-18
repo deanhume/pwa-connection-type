f0982dfd256a78de09dea9c836eb89a8fad2c6eb
Dean <deanhume@gmail.com>
2018-07-18 14:16:26 +0100

    Added missing (

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@
 self.addEventListener('fetch', function (event) {
 
     // Check if the current request is slow 3g
-    if (/\slow-2g|2g/.test(navigator.connection.effectiveType) {
+    if (/\slow-2g|2g/.test(navigator.connection.effectiveType)) {
         // Check if the request is for an image
         if (/\.jpg$|.png$|.gif$|.webp$/.test(event.request.url)) {
 
